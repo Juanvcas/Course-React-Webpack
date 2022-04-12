ccbe5b2ecdf2e3c14883f4a64d30d5d964c301ce
Juanvcas <juanvcas96@gmail.com>
2022-04-11 22:47:29 -0500

    Configured plugins and loaders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
-const App = () => <h1>Hello React!</h1>;
+const App = () => <div>
+    <h1>Hello React!!!</h1>
+    <h2>This is the first practice using React!</h2>
+</div>;
 
 export default App;
