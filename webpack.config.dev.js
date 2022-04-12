6124b65f2168a1231b5f28a01c28e20867380b93
Juanvcas <juanvcas96@gmail.com>
2022-04-12 08:55:50 -0500

    Configured mode development and production

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,24 +1,17 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname,"dist"),
         filename: "bundle.js",
-        publicPath: "/"
     },
     resolve: {
-        extensions: [".js",".jsx"],
-        alias: {
-            "@components": path.resolve(__dirname,"src/components/"),
-            "styles": path.resolve(__dirname,"src/styles/")
-        }
+        extensions: [".js",".jsx"]
     },
-    mode: "production",
+    mode: "development",
     module: {
         rules: [
             {
@@ -50,13 +43,16 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: "[name].css"
-        }),
+        })
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin()
-        ]
+    devServer: {
+        static: {
+            directory: path.join(__dirname,"dist"),
+            watch: true
+        },
+        watchFiles: ["./**"],
+        compress: true,
+        port: 8080,
+        open: true
     }
 }
